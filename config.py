c89d02a3a04009438adfbccd72da3852570b8733
Haykal20 <thehaykal219@gmail.com>
2026-06-19 16:23:12 +0700

    fix: hapus kunci rahasia hardcoded dan tambah dependensi keamanan

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     # Pakai SQLite lokal, langsung otomatis bikin file 'finance.db' di folder lo
     SQLALCHEMY_DATABASE_URI = 'sqlite:///finance.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ganti-dengan-string-random-yang-panjang-untuk-keamanan-2024'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     
     # Pengaturan keamanan session
     SESSION_COOKIE_SECURE = False  # Set True jika pakai HTTPS
